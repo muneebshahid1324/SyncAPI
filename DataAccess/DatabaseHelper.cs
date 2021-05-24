ea9de0293309c219f8bdf72c12c40e95e7319878
muneeb <muneeb.techsolab@hotmail.com>
2021-05-24 13:34:10 +0500

    Update

---
+++ DataAccess/DatabaseHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Linq;
+using System.Data.Linq.Mapping;
+using System.Linq;
+using System.Reflection;
+using System.Security.Principal;
+using System.Web;
+
+public abstract class EntityBase
+{
+    internal virtual void OnSaving(ChangeAction changeAction) { }
+
+    internal virtual void OnSaved() { }
+}
+
+internal class ChangeEntity
+{
+    public ChangeAction ChangeAction { get; set; }
+
+    public EntityBase Entity { get; set; }
+}
